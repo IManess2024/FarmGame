d822f4e74c6891c6e60659098934ba2ee4842596
ManessMadness <manessmadnessbusiness@gmail.com>
2026-05-09 10:45:26 +0100

    Added turkey sprite and movement update

--- main.cpp
+++ main.cpp
@@ -1,46 +1,44 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
+sf::Vector2i framesize(32,32);
+    float animationFrameTime = 0.12f;
+
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML Template");
     window.setFramerateLimit(60);
 
-    sf::CircleShape circle(80.f);
-    circle.setFillColor(sf::Color(100, 180, 255));
-    circle.setPosition({260.f, 180.f});
+    
+
+    
 
     sf::Texture turkey_sprite_sheet;
     if(!turkey_sprite_sheet.loadFromFile("Textures/Turkey_animation_with_shadow.png"))
     {
         //Handle the error
         std::cout << "ERROR" << std::endl;
+        return 1;
 
     }
 
-    sf::Sprite turkey(turkey_sprite_sheet);
-    turkey.setPosition({0.0f, 0.0f});
+    sf::Sprite turkey(turkey_sprite_sheet, sf::IntRect({0,0}, framesize));
 
+    turkey.setScale({4.f,4.f});
+turkey.setPosition({260.f, 180.f});
 
-    sf::IntRect frame_1({320, 1232},{28, 32});
-    sf::IntRect frame_2({320, 1232},{28, 32});
-    sf::IntRect frame_3({320, 1232},{28, 32});
-    sf::IntRect frame_4({320, 1232},{28, 32});
-    sf::IntRect frame_5({320, 1232},{28, 32});
-
-    std::vector<sf::IntRect> all_frames;
-    all_frames.push_back(frame_1);
-    all_frames.push_back(frame_2);
-    all_frames.push_back(frame_3);
-    all_frames.push_back(frame_4);
-    all_frames.push_back(frame_5);
+   
 
     unsigned int idx = 0;
     float anim_timer = 0.f;
     float const anim_speed = 1.0f;
-    
+    int animationframe = 0;
 
+int downRow = 0;
+     int upRow = 1;
+     int leftRow = 2;
+     int rightRow = 3;
 
 
 
@@ -49,7 +47,7 @@ int main()
 
 float speed = 300.0f;
 sf::Clock clock;
-
+int directionrow = 0;
 
     while (window.isOpen())
     {
@@ -72,30 +70,54 @@ float dt = clock.restart().asSeconds();
         {
             // left key is pressed: move our character
             movement.x -= speed*dt;
+            directionrow = leftRow;
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
         {
             // left key is pressed: move our character
             movement.y -= speed*dt;
+            directionrow = upRow;
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
         {
             // left key is pressed: move our character
             movement.y += speed*dt;
+            directionrow = downRow;
+
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
         {
             // left key is pressed: move our character
             movement.x += speed*dt;
+            directionrow = rightRow;
         }
-circle.move(movement);
+
+// Animate only while moving.
+        if (movement.x != 0.f || movement.y != 0.f)
+        {
+            anim_timer += dt;
+            if (anim_timer >= animationFrameTime)
+            {
+                anim_timer = 0.f;
+                animationframe = (animationframe + 1) % 6;
+            }
+        }
+        else
+        {
+            animationframe = 0;
+            anim_timer = 0.f;
+        }
+
+turkey.move(movement);
+turkey.setTextureRect(sf::IntRect({animationframe*framesize.x, directionrow*framesize.y}, framesize));
 
 
         //Final operations
 
         window.clear(sf::Color(30, 30, 40));
         window.draw(turkey);
-        window.draw(circle);
         window.display();
     }
+
+    return 0;
 }
